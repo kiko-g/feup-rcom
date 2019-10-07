d7f2002066013284fba0e7d8786dd729da227921
Francisco Gonçalves <40745490+kiko-g@users.noreply.github.com>
2019-10-07 10:12:36 +0100

    Add files via upload

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -1,11 +1,14 @@
 /*Non-Canonical Input Processing*/
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
 
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
@@ -13,103 +16,92 @@
 #define FALSE 0
 #define TRUE 1
 
-volatile int STOP=FALSE;
-int main(int argc, char** argv)
-{
-    int fd,c, res;
-    struct termios oldtio, newtio;
-    char buf[255];
-    int i, sum = 0, speed = 0;
-    
-    if ( (argc < 2) || 
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
-    }
+volatile int STOP = FALSE;
 
+int main(int argc, char **argv)
+{
+  int fd, c, res;
+  struct termios oldtio, newtio;
+  char buf[255];
+  int i, sum = 0, speed = 0;
+
+  if ((argc < 2) ||
+      ((strcmp("/dev/ttyS0", argv[1]) != 0) &&
+       (strcmp("/dev/ttyS1", argv[1]) != 0) &&
+       (strcmp("/dev/pts/5", argv[1]) != 0)))
+  {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+    exit(1);
+  }
 
   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
   */
 
+  fd = open(argv[1], O_RDWR | O_NOCTTY);
+  if (fd < 0)
+  {
+    perror(argv[1]);
+    exit(-1);
+  }
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+  if (tcgetattr(fd, &oldtio) == -1)
+  { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
 
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
 
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
-
-
+  newtio.c_cc[VTIME] = 0; /* inter-character timer unused */
+  newtio.c_cc[VMIN] = 5;  /* blocking read until 5 chars received */
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) próximo(s) caracter(es)
+    leitura do(s) prï¿½ximo(s) caracter(es)
   */
 
+  tcflush(fd, TCIOFLUSH);
 
+  if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+  {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
-    tcflush(fd, TCIOFLUSH);
+  printf("New termios structure set\n");
 
-    if (tcsetattr(fd,TCSANOW,&newtio) == -1)
-	{
-      	perror("tcsetattr");
-      	exit(-1);
-    }
+  gets(buf);
 
-    printf("\nNew termios structure set\n\n");
-	
-	char palavraEnviar[12];
-	printf("Palavra a enviar: ");
-	gets(palavraEnviar);
+  int nrCharacters = 1;
 
-	for(i=0; i<strlen(palavraEnviar); i++)
-	{
-		buf[i] = palavraEnviar[i];
-	}
+  for (int i = 0; i < 255; i++)
+    if (buf[i] == '\0')
+      break;
+    else
+      nrCharacters++;
 
-	
-    //for (i = 0; i < 255; i++)
-    //buf[i] = 'a';
-    
-    /*testing*/
-    buf[25] = '\n';
-    
-    res = write(fd,buf,strlen(palavraEnviar));   
-    printf("%d bytes written\n", res);
-	
+  res = write(fd, buf, nrCharacters);
+  printf("%d bytes written\n", res);
 
-    /* 
-    O ciclo FOR e as instruções seguintes devem ser alterados de modo a respeitar 
-    o indicado no guião 
-	*/
+  /* 
+    O ciclo FOR e as instruï¿½ï¿½es seguintes devem ser alterados de modo a respeitar 
+    o indicado no guiï¿½o 
+  */
 
+  if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+  {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
-	sleep(1);
-   
-    if(tcsetattr(fd,TCSANOW,&oldtio) == -1)
-	{
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-
-
-
-    close(fd);
-    return 0;
+  close(fd);
+  return 0;
 }
