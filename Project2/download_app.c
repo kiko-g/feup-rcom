1e2378c74f1e445e4bee058948ecc7819653b417
kiko-g <kikojpgoncalves@gmail.com>
2019-11-25 13:06:21 +0000

    starting

---
+++ Project2/download_app.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+
+
+
+int checkUsage(int n, char* str);
+int main(int argc, char **argv)
+{
+    if(checkUsage(argc, argv[0])) return 1;
+
+}
+
+
+
+
+
+int checkUsage(int n, char* str)
+{
+    if (n != 2)
+    {
+        printf("Wrong executable call (number of arguments must be 2).\n");
+        printf("====== USAGE ======\nNORMAL\t %s ftp://[<user>:<password>@]<host>/<url-path>\n", str);
+        printf("ANONYMOUS\t %s ftp://<host>/<url-path>\n\n\n", str);
+        return 1;
+    }
+    
+    return 0;
+}
